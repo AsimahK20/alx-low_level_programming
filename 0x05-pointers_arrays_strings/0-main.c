2ed2617c44fe8d19d16c4d0627ce254aa927062b
Asimah Khan <Asimahk20@gmail.com>
2023-02-28 19:21:20 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,19 +1,18 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - check my code
+ * main - check the code 
  *
- * Return: 0
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int n;
+    int n;
 
-	n = 402;
-	printf("n=%d\n",n);
-	reset_to_98(&n);
-	printf("n=%d\n",n);
-	return (0);
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
 }
